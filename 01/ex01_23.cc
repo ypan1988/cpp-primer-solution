672e0700bac07511e18c1242349bb194f39d7e75
yipan <ypan1988@gmail.com>
2016-05-21 22:57:23 +0100

    add ex01_23

---
+++ 01/ex01_23.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+  Sales_item currItem, item;
+
+  if (std::cin >> currItem) {
+    int cnt = 1;
+    while (std::cin >> item) {
+      if (item.isbn() == currItem.isbn())
+        ++cnt;
+      else {
+        std::cout << currItem.isbn() << " occurs "
+                  << cnt << " times " << std::endl;
+        currItem = item;
+        cnt = 1;          
+      }
+    } // while loop ends here
+    std::cout << currItem.isbn() << " occurs "
+              << cnt << " times " << std::endl;
+  }
+
+  return 0;
+}
