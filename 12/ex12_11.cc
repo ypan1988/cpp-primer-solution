d8fefddd1801f47c3ae2aa7badaee8123c4be2fd
Yi Pan <ypan1988@gmail.com>
2016-07-27 16:01:41 +0100

    add ex12_10 ex12_11

---
+++ 12/ex12_11.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <memory>
+
+void process(std::shared_ptr<int> ptr)
+{
+  std::cout << "inside the process function:"
+            << ptr.use_count() << std::endl;
+}
+
+int main()
+{
+  std::shared_ptr<int> p(new int(42));
+  std::cout << p.use_count() << "\n";
+  process(std::shared_ptr<int>(p.get()));
+
+  /**
+   * codes below shows how the reference count change.
+   */
+  std::cout << p.use_count() << "\n";
+  auto q = p;
+  std::cout << p.use_count() << "\n";
+  std::cout << "the int p now points to is:" << *p << "\n";
+  return 0;
+}
