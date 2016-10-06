7730ebd0c276616f87f65c4097d7bb2ca288552c
Yi Pan <ypan1988@gmail.com>
2016-10-06 16:40:53 +0100

    update ex02_41

---
+++ 02/ex02_41a.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+struct Sales_data {
+  std::string bookNo;
+  unsigned units_sold = 0;
+  double revenue = 0.0;
+};
+
+int main()
+{
+  Sales_data total;
+  double price;
+
+  if (std::cin >> total.bookNo >> total.units_sold >> price) {
+    total.revenue = total.units_sold * price; 
+    Sales_data trans;
+    while (std::cin >> trans.bookNo >> trans.units_sold >> price) {
+      trans.revenue = trans.units_sold * price;
+      total.units_sold += trans.units_sold;
+      total.revenue += trans.revenue; 
+    }
+    std::cout << total.bookNo << " "
+	      << total.units_sold << " "
+	      << total.revenue<< std::endl;
+  } else {
+    std::cerr << "No Data?!" << std::endl;
+    return -1;
+  }
+
+  return 0;
+}
