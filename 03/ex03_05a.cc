a29e8f084683442dc4d53b110463c3a926fd4745
Yi Pan <ypan1988@gmail.com>
2016-10-08 15:43:13 +0100

    add ex03_05

---
+++ 03/ex03_05a.cc
@@ -0,0 +1,16 @@
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::cin; using std::cout; using std::endl;
+
+int main()
+{
+  string word, s;
+  while (cin >> word)
+    s += word;
+  
+  cout << "s: " << s  << endl;
+  
+  return 0;
+}
