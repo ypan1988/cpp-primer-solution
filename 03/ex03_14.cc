00a102611aa88134952f1b89e9a79d49deae5ebf
Yi Pan <ypan1988@gmail.com>
2016-10-12 14:00:51 +0100

    add ex03_14

---
+++ 03/ex03_14.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cin;
+
+#include <vector>
+using std::vector;
+
+int main()
+{
+  int i;
+  vector<int> v1;
+
+  while (cin >> i)
+    v1.push_back(i);
+
+  return 0;
+}
