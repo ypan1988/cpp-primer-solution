7363eccac52888a19d708605af6a3af059f2a7b3
Yi Pan <ypan1988@gmail.com>
2016-10-16 10:54:31 +0100

    add ex03_31

---
+++ 03/ex03_31.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cout; using std::endl;
+
+int main()
+{
+  int ia[10];
+  for (auto i = 0; i != 10; ++i) ia[i] = i;
+  for (auto i : ia) cout << i << " ";
+  cout << endl;
+  
+  return 0;
+}
