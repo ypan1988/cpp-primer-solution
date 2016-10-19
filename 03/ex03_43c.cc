007187e6ac094e1a401be4c10849d77998c48540
Yi Pan <ypan1988@gmail.com>
2016-10-19 16:36:47 +0100

    add ex03_43

---
+++ 03/ex03_43c.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using std::cout; using std::endl;
+
+#include <iterator>
+using std::begin; using std::end;
+
+int main()
+{
+  int ia[3][4] = {		// three elements; each element is an array of
+				// size 4
+    {0, 1, 2, 3},		// initializers for the row indexed by 0
+    {4, 5, 6, 7},		// initializers for the row indexed by 1
+    {8, 9, 10, 11}		// initializers for the row indexed by 2
+  };
+
+  for (int (*p1)[4] = begin(ia); p1 != end(ia); ++p1)
+    for (int *p2 = begin(*p1); p2 != end(*p1); ++p2)
+      cout << *p2 << endl;
+  
+  return 0;
+}
