989f4ad28991a9685b65327003ec798c60b74bc1
Yi Pan <ypan1988@gmail.com>
2016-10-19 17:01:38 +0100

    add ex03_45

---
+++ 03/ex03_45a.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using std::cout; using std::endl;
+
+int main()
+{
+  int ia[3][4] = {		// three elements; each element is an array of
+				// size 4
+    {0, 1, 2, 3},		// initializers for the row indexed by 0
+    {4, 5, 6, 7},		// initializers for the row indexed by 1
+    {8, 9, 10, 11}		// initializers for the row indexed by 2
+  };
+
+  for (auto &row : ia)
+    for (auto &col : row)
+      cout << col << endl;
+  
+  return 0;
+}
