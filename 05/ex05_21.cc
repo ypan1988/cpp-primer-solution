2cb5e9c3f5c4ed9c9726729b97d163387c6ed2e9
Yi Pan <ypan1988@gmail.com>
2016-11-03 15:09:30 +0000

    add ex05_22

---
+++ 05/ex05_21.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using std::cin; using std::cout; using std::endl;
+
+#include <string>
+using std::string;
+
+#include <cctype>
+using std::isupper;
+
+int main()
+{
+  string word, word2;
+  bool no_repeated_word = true; 
+  while (cin >> word) {
+    if (!isupper(word[0])) {
+      word2 = word;
+      continue;
+    }
+    
+    if (word == word2) {
+      no_repeated_word = false;
+      cout << word << endl;
+      break;
+    } else {
+      word2 = word; 
+    }
+  }
+
+  if (no_repeated_word) cout << "no word was repeated." << endl;
+  
+  return 0;
+}
