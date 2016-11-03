16d5ac04bfaa41e5eae397690792dfd36a0b9e93
Yi Pan <ypan1988@gmail.com>
2016-11-03 21:32:02 +0000

    add ex06_03

---
+++ 06/ex06_03.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::cout; using std::endl;
+
+// factorial of val is val*(val-1)*(val-2)...*2*1
+int fact(int val)
+{
+  int ret = 1;			// local variable to hold the results as we
+				// calculate it
+  while (val > 1)
+    ret *= val--;		// assign ret * val to ret and decrement val
+  return ret;			// return the result;
+}
+
+int main()
+{
+  int j = fact(5);		// j equals 120, i.e., the result of fact(5)
+  cout << "5! is " << j << endl;
+
+  return 0;
+}
