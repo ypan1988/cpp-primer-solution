53ef5c86531a3c37dde28cc274516a4e1e4c25ea
Yi Pan <ypan1988@gmail.com>
2017-04-07 16:20:02 +0100

    add some comments

--- 09/ex09_24.cc
+++ 09/ex09_24.cc
@@ -9,9 +9,9 @@ int main()
   vector<int> ivec;
 
   cout << ivec.at(0) << endl;
-  cout << ivec[0] << endl;
-  cout << ivec.front() << endl;
-  cout << *ivec.begin() << endl;
+  cout << ivec[0] << endl;       // Segmentation fault
+  cout << ivec.front() << endl;  // Segmentation fault
+  cout << *ivec.begin() << endl; // Segmentation fault
 
   return 0;
 }
