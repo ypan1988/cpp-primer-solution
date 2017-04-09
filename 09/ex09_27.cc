07127bd28e56edef43141539118d9992c7e97dba
Yi Pan <ypan1988@gmail.com>
2017-04-09 14:09:48 +0100

    add ex09_27

---
+++ 09/ex09_27.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using std::cout; using std::endl;
+
+#include <forward_list>
+using std::forward_list;
+
+int main()
+{
+  forward_list<int> ilst = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+  auto prev = ilst.before_begin();
+  auto curr = ilst.begin();
+  while (curr != ilst.end()) {
+    if (*curr % 2) curr = ilst.erase_after(prev);
+    else {
+      prev = curr;
+      ++curr;
+    }
+  }
+
+  cout << "forward_list: ";
+  for (const auto &ival : ilst)
+    cout << ival << " ";
+  cout << endl;
+
+  return 0;
+}
