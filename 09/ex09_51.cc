77e5d1d8e9661b10270ad2bd6335c509eee39242
Yi Pan <ypan1988@gmail.com>
2017-04-20 23:31:56 +0100

    update ex09_51.cc

--- 09/ex09_51.cc
+++ 09/ex09_51.cc
@@ -6,11 +6,31 @@ using std::string;
 
 class Date {
 private:
-  unsigned year_, month_, day_;
+  unsigned year_ = 2000;
+  unsigned month_ = 1;
+  unsigned day_ = 1;
 
 public:
   Date(const string &s) {
+    const string num("0123456789");
+    const string sep("/");
 
+    // January 1, 2000 or Jan 1, 2000
+    if (s.find_first_of(',') != string::npos) {
+      string mm = s.substr(0, s.find_first_of(' '));
+      if (mm == "Jan" || mm == "January") month_ = 1;
+      else if (mm == "Feb" || mm == "February") month_ = 2;
+      else if (mm == "Mar" || mm == "March") month_ = 3;
+      else if (mm == "Apr" || mm == "April") month_ = 4;
+      else if (mm == "May") month_ = 5;
+      else if (mm == "Jun" || mm == "June") month_ = 6;
+      else if (mm == "Jul" || mm == "July") month_ = 7;
+      else if (mm == "Aug" || mm == "August") month_ = 8;
+    }
+    
+    // 1/1/2000
+    if (s.find_first_of('/') != string::npos) {
+    }
   }
 
   void print() {
@@ -23,14 +43,14 @@ public:
 
 int main()
 {
-  Data data1("January 1, 1900");
-  data1.print();
+  Date date1("August 1, 1900");
+  date1.print();
   
-  Data data2("1/1/1900");
-  data2.print();
+  Date date2("1/1/1900");
+  date2.print();
 
-  Data data3("Jan 1, 1900");
-  data3.print();
+  Date date3("Aug 1, 1900");
+  date3.print();
 
   return 0;
 }
