a7b567b236de42ce0b955f99ae94277cf57d9715
Yi Pan <ypan1988@gmail.com>
2017-04-20 14:53:35 +0100

    add ex09_44

---
+++ 09/ex09_44.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using std::cout; using std::endl;
+
+#include <string>
+using std::string;
+
+void Replace(string &s, const string &oldVal, const string &newVal)
+{
+  string::size_type index = 0;
+  while (s.size() - index >= oldVal.size()) {
+    if (s.substr(index, oldVal.size()) == oldVal) {
+      s.replace(index, oldVal.size(), newVal);
+      index += newVal.size();
+    } else ++index;
+  }
+}
+
+int main()
+{
+  string str{"hello thru world tho !!!"};
+  Replace(str, "thru", "through");
+  Replace(str, "tho", "though");
+
+  std::cout << str << std::endl;
+
+  return 0;
+}
