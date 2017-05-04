f77aeb2a3224490dd404850286adb3720b0e24ae
Yi Pan <ypan1988@gmail.com>
2017-05-04 22:49:51 +0100

    add ex10_32

--- 10/ex10_32.cc
+++ 10/ex10_32.cc
@@ -1,23 +1,41 @@
 #include <iostream>
 using std::cin;
+using std::cout;
+using std::endl;
 
 #include <vector>
 using std::vector;
 
 #include <algorithm>
 using std::sort;
+using std::find_if;
+using std::accumulate;
+
+#include <iterator>
+using std::istream_iterator;
 
 #include "Sales_item.h"
 
-bool compareIsbn
 int main()
 {
-  istream_iterator<Sales_iterm> in_iter(cin), eof;
-  vector<Sales_iterm> vec(in_iter, eof);
+  istream_iterator<Sales_item> in_iter(cin), eof;
+  vector<Sales_item> vec(in_iter, eof);
 
   sort(vec.begin(), vec.end(),
-       [](const Sales_data &obj1, const Sales_data &obj2)
+       [](const Sales_item &obj1, const Sales_item &obj2)
        { return obj1.isbn() < obj2.isbn(); });
+
+  Sales_item sum;
+  auto iter = vec.cbegin();
+  while (iter != vec.cend()) {
+    auto start = iter;
+    auto end = vec.cend();
+    end = find_if(start, end,
+		  [start](const Sales_item &trans) 
+		  { return trans.isbn() != start->isbn(); });
+    cout << accumulate(start, end, Sales_item(start->isbn())) << endl;
+    iter = end;
+  }
 
   return 0;
 }
