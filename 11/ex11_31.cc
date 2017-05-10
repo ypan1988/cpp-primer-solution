2a94f1efeeee21b6125ce16545442b1bcbb720dd
Yi Pan <ypan1988@gmail.com>
2017-05-10 14:02:18 +0100

    add ex11_31

--- 11/ex11_31.cc
+++ 11/ex11_31.cc
@@ -5,7 +5,7 @@ using std::cout; using std::endl;
 using std::string;
 
 #include <map>
-using std::map;
+using std::multimap;
 
 int main()
 {
@@ -14,8 +14,53 @@ int main()
   // add data to authors
   authors.insert({"Alain de Botton", "On Love"});
   authors.insert({"Alain de Botton", "Status Anxiety"});
+  authors.insert({"Stanley B. Lippman", "Inside the C++ Object Model"});
+  authors.insert({"Stanley B. Lippman", "C++ Primer"});
   authors.insert({"Alain de Botton", "Art of Travel"});
   authors.insert({"Alain de Botton", "Architecture of Happiness"});
+
+  for (const auto &p : authors) {
+    cout << p.first << " "
+	 << p.second << endl;
+  }
+  cout << endl;
+
+  string search_item("Stanley B. Lippman"); // author we'll look for
+  string book("C++ Primer");		    // the book we'll look for
+  auto entries = authors.count(search_item); // number of elements
+  auto iter = authors.find(search_item);     // first entry for this author
+  while (entries) {
+    if (iter->second == book) {
+      authors.erase(iter);
+      break;
+    }
+    ++iter;
+    --entries;
+  }
+
+  for (const auto &p : authors) {
+    cout << p.first << " "
+	 << p.second << endl;
+  }
+  cout << endl;
+
+  book = "C++ Primer Plus";
+  entries = authors.count(search_item); // number of elements
+  iter = authors.find(search_item);     // first entry for this author
+  while (entries) {
+    if (iter->second == book) {
+      authors.erase(iter);
+      break;
+    }
+    ++iter;
+    --entries;
+  }
+
+  for (const auto &p : authors) {
+    cout << p.first << " "
+	 << p.second << endl;
+  }
+  cout << endl;
 
   return 0;
 }
