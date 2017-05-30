760d742deea42cefde96b18b2a87e969c525b243
Yi Pan <ypan1988@gmail.com>
2017-05-30 10:51:09 +0100

    add ex13_30

---
+++ 13/ex13_30.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+class HasPtr {
+  friend void swap(HasPtr&, HasPtr&);
+ public:
+  HasPtr(const std::string &s = std::string()):
+    ps(new std::string(s)), i(0) { }
+  // each HasPtr has its own copy of the string to which ps points
+  HasPtr(const HasPtr &p):
+    ps(new std::string(*p.ps)), i(p.i) { }
+  HasPtr& operator=(const HasPtr &rhs) {
+    auto newp = new std::string(*rhs.ps);  // copy the underlying string
+    delete ps;    // free the old memory
+    ps = newp;    // copy data from rhs into this object
+    i = rhs.i;
+    return *this; // return this object
+  }
+  ~HasPtr() { delete ps; }
+ private:
+  std::string *ps;
+  int i;
+};
+
+inline 
+void swap(HasPtr &lhs, HasPtr &rhs)
+{
+  std::cout << "inside swap(HasPtr &lhs, HasPtr &rhs)" << std::endl;
+  using std::swap;
+  swap(lhs.ps, rhs.ps);		// swap the pointers, not the string data
+  swap(lhs.i, rhs.i);		// swap the int member
+}
+
+int main()
+{
+  HasPtr hp1("hello"), hp2("world");
+  swap(hp1, hp2);
+
+  return 0;
+}
