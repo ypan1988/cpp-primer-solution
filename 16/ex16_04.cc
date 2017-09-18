a65749540ccf3c2e10910391f54243df7e85dc55
Yi Pan <ypan1988@gmail.com>
2017-09-18 11:09:00 +0100

    update ex16_04

--- 16/ex16_04.cc
+++ 16/ex16_04.cc
@@ -10,8 +10,8 @@ using std::vector;
 #include <list>
 using std::list;
 
-template <typename Iterator, typename Valuetype>
-Iterator find(Iterator first, Iterator last, const Valuetype &value)
+template <typename iterator, typename value_type>
+iterator find(iterator first, iterator last, const value_type &value)
 {
   while (*first != value && first != last) ++first;
   return first;
@@ -22,10 +22,10 @@ int main()
   vector<int> ivec = {1, 2, 3, 4, 5, 6, 7, 8, 9};
   cout << (ivec.cend() != find(ivec.begin(), ivec.end(), 5) ? "found" : "not found")
        << endl;
-  
+
   list<string> slst = {"aa", "bb", "cc", "dd", "ee", "ff", "gg"};
   cout << (slst.cend() != find(slst.begin(), slst.end(), "zz") ? "found" : "not found")
        << endl;
-  
+
   return 0;
 }
