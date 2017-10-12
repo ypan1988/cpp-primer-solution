2e9afd6441585630419c731672ee69daed68f748
Yi Pan <ypan1988@gmail.com>
2017-10-12 10:48:57 +0100

    add ex16_51 & ex16_52

---
+++ 16/ex16_52.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+template <typename T, typename... Args>
+void foo(const T &t, const Args& ... rest)
+{
+  std::cout << sizeof...(Args) << " "
+	    << sizeof...(rest) << std::endl;
+}
+
+int main()
+{
+  int i = 0; double d = 3.14; std::string s = "how now brown cow";
+  foo(i, s, 42, d);
+  foo(s, 42, "hi");
+  foo(d, s);
+  foo("hi");
+
+  return 0;
+}
